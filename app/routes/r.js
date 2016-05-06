25a13314c04cd998a2d3c9fecb14b56d72906b78
Barnabas Bulpett <bbulpett@charter.net>
2016-05-05 23:06:40 -0400

    Add model and aftermodel to "r" routes

--- app/routes/r.js
+++ app/routes/r.js
@@ -1,4 +1,12 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+	model: function(params){
+
+		return Ember.$.getJSON(`https://www.reddit.com/r/${params.subreddit}.json`);
+	},
+
+	afterModel: function(r){
+		r.subreddit = r.data.children[0].data.subreddit || '';
+	}
 });
